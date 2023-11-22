95631bf826ffbd147e079cfd528fc753031eb000
Timofey <timafolf2003@mail.ru>
2023-11-22 14:42:34 +0300

    add multiplyMatrices method in Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,35 @@ import java.util.Random;
 
 class Main{
     public static void main(String[] args) {
-        System.out.print("Это добавлено в ветке main");
+        int size = 3;
+        int[][] firstMatrix = new int[size][size];
+        int[][] secondMatrix = new int[size][size];
+        fillMatrix(firstMatrix);
+        fillMatrix(secondMatrix);
+        int[][] unitMatrix = getUnitMatrix(size);
+        printMatrix(firstMatrix);
+        System.out.println();
+        printMatrix(secondMatrix);
+        System.out.println();
+        int[][] result = multiplyMatrices(firstMatrix, secondMatrix);
+        printMatrix(result);
     }
+
+    public static int[][] multiplyMatrices(int[][] matrixA, int[][] matrixB) {
+        int rowsA = matrixA.length;
+        int colsA = matrixA[0].length;
+        int colsB = matrixB[0].length;
+        int[][] result = new int[rowsA][colsB];
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsB; j++) {
+                for (int k = 0; k < colsA; k++) {
+                    result[i][j] += matrixA[i][k] * matrixB[k][j];
+                }
+            }
+        }
+        return result;
+    }
+
     public static int[][] getUnitMatrix(int size) {
         int[][] unitMatrix = new int[size][size];
         for (int i = 0; i < unitMatrix.length; i++) {
@@ -21,7 +48,7 @@ class Main{
         Random random = new Random();
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                matrix[i][j] = random.nextInt(100, 1000);
+                matrix[i][j] = random.nextInt(1, 10);
             }
         }
     }
