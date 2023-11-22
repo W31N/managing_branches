d32d1c1dcbf4872f0ff72a383230a11ee6cb8800
Timofey <timafolf2003@mail.ru>
2023-11-22 14:41:05 +0300

    add getUnitMatrix method in Main.java

--- Main.java
+++ Main.java
@@ -2,4 +2,17 @@ class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
     }
+    public static int[][] getUnitMatrix(int size) {
+        int[][] unitMatrix = new int[size][size];
+        for (int i = 0; i < unitMatrix.length; i++) {
+            for (int j = 0; j < unitMatrix[i].length; j++) {
+                if (i == j) {
+                    unitMatrix[i][j] = 1;
+                } else {
+                    unitMatrix[i][j] = 0;
+                }
+            }
+        }
+        return unitMatrix;
+    }
 }
