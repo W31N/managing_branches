1170d311a68979a1b36ea6b67097962b6340b97c
Timofey <timafolf2003@mail.ru>
2023-11-22 14:41:29 +0300

    add fillMatrix method in Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
@@ -14,5 +16,13 @@ class Main{
             }
         }
         return unitMatrix;
+    }
+    public static void fillMatrix(int[][] matrix) {
+        Random random = new Random();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = random.nextInt(100, 1000);
+            }
+        }
     }
 }
